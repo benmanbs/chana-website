b29297269dc17cda9a727fe34555a7656452ef80
Benjamin Shai <bshai@conductor.com>
2018-10-21 12:13:28 -0400

    initial commit, simple web server for testing and skeleton setup

---
+++ simple-server/index.js
@@ -0,0 +1,10 @@
+/**
+ * @author bshai date 10/21/18.
+ */
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('../static'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
